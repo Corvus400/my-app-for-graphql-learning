3bf03d4960a370e923877d9128cecb250563f22d
todayama_r <13657682+Corvus400@users.noreply.github.com>
2022-01-09 20:52:51 +0900

    :sparkles Add Apolloクライアントの依存関係を追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    id("com.apollographql.apollo3").version("3.0.0")
 }
-
-val composeVersion: String by extra
 
 android {
     defaultConfig {
@@ -38,7 +37,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = composeVersion
+        kotlinCompilerExtensionVersion = "1.0.5"
     }
     packagingOptions {
         resources {
@@ -52,14 +51,19 @@ dependencies {
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.4.0")
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.compose.ui:ui:$composeVersion")
-    implementation("androidx.compose.material:material:$composeVersion")
-    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.compose.ui:ui:1.0.5")
+    implementation("androidx.compose.material:material:1.0.5")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.0.5")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
     implementation("androidx.activity:activity-compose:1.4.0")
     testImplementation("junit:junit:")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
-    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.5")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.0.5")
+    implementation("com.apollographql.apollo3:apollo-runtime:3.0.0")
+}
+
+apollo {
+    packageName.set("com.todayamar.myappforgraphqllearning")
 }
